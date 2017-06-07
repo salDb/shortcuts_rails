0c007d87c94bff50296d7cfa664604c6043a417d
sal <xpriceex@gmail.com>
2017-06-07 21:47:13 +0100

    app model paperclip configurations

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,5 +1,5 @@
 class App < ApplicationRecord
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image
   validates_attachment :image, presence: true
   do_not_validate_attachment_file_type :image
 
