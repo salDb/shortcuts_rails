1351a7446ad70bd7c31009a9f6cac12315789308
Sal <xpriceex@gmail.com>
2017-07-16 11:38:26 +0100

    removed uniqueness from shortcut model

--- app/models/shortcut.rb
+++ app/models/shortcut.rb
@@ -4,6 +4,5 @@ class Shortcut < ApplicationRecord
   validates :action_name, presence: true, length: {minimum: 3}
   validates :first_action, presence: true
   validates :second_action, presence: true
-  validates_uniqueness_of :action_name, case_sensitive: false
 
 end
