cdb49780afe6a0aff637ae04819c1315883ab2f9
Sal <xpriceex@gmail.com>
2017-07-16 02:04:27 +0100

    added case sensitive uniqueness to shortcut model

--- app/models/shortcut.rb
+++ app/models/shortcut.rb
@@ -1,7 +1,9 @@
 class Shortcut < ApplicationRecord
   belongs_to :app
 
-  validates :action_name, presence: true, length: {minimum: 3}, uniqueness: true
+  validates :action_name, presence: true, length: {minimum: 3}
   validates :first_action, presence: true
   validates :second_action, presence: true
+  validates_uniqueness_of :action_name, case_sensitive: false
+
 end
