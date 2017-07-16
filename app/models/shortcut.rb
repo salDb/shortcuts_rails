440cbd5cbc60f7300f1a161398d6c05d6d0753a0
Sal <xpriceex@gmail.com>
2017-07-16 12:34:48 +0100

    upcasing shortcuts before saving them

--- app/models/shortcut.rb
+++ app/models/shortcut.rb
@@ -1,8 +1,16 @@
 class Shortcut < ApplicationRecord
   belongs_to :app
+  before_save :upcase_shortcut
 
   validates :action_name, presence: true, length: {minimum: 3}
   validates :first_action, presence: true
   validates :second_action, presence: true
+
+  def upcase_shortcut
+    self.first_action.upcase!
+    self.second_action.upcase!
+    self.third_action.upcase!
+    self.fourth_action.upcase!
+  end
 
 end
